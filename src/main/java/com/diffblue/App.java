e66d37676ae9aba1a0db66e28703a4a76df06359
Paul Kent <paulkent@Pauls-MacBook-Pro.local>
2017-09-27 11:49:50 +0100

    added is it real

--- src/main/java/com/diffblue/App.java
+++ src/main/java/com/diffblue/App.java
@@ -6,7 +6,7 @@ public class App
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("How many lottery balls would you like to play?");
+        System.out.println("How many lottery balls would you like to play 6 or 7?");
         int n = sc.nextInt();
         int[] lottery = new int[n];
         int randomNum;
@@ -25,6 +25,24 @@ public class App
 
         for (int i = 0; i < lottery.length; i++)
             System.out.print(lottery[i] + " ");
+
+        isItReal(lottery);
+
+    }
+
+    public static boolean isItReal(int[] lottery) {
+        boolean real;
+        if (lottery.length < 8) {
+            System.out.println("Its a real lottery, winner winner chicken dinner!");
+            real = true;
+            return real;
+        }
+        else {
+            System.out.println("Its not a real lottery you idiothole");
+            real = false;
+            return false;
+
+        }
 
     }
 }
