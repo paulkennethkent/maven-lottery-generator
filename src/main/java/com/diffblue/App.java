920406739eb0735c6328e3f6757e8b5e34477400
Paul Kent <paulkent@Pauls-MacBook-Pro.local>
2017-09-27 11:33:29 +0100

    user input

--- src/main/java/com/diffblue/App.java
+++ src/main/java/com/diffblue/App.java
@@ -1,19 +1,21 @@
 package com.diffblue;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class App
 {
     public static void main(String[] args) {
-
-        int[] lottery = new int[6];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many lottery balls would you like to play?");
+        int n = sc.nextInt();
+        int[] lottery = new int[n];
         int randomNum;
-
-        for (int i = 0; i < 6; i++) {
-            randomNum = (int) (Math.random() * 49 +1);
+        for (int i = 0; i < lottery.length; i++) {
+            randomNum = (int) (Math.random() * 49);
             for (int x = 0; x < i; x++) {
                 if (lottery[x] == randomNum)
                 {
-                    randomNum = (int) (Math.random() * 49 +1);
+                    randomNum = (int) (Math.random() * 49);
                 }
 
             }
